38350756715d3868bc9151b19595c70c167e5e3f
Gleb <gleb.berezkin.2017@gmail.com>
2022-10-27 18:00:21 +0300

    Add new test IS and Mvc

---
+++ Test IS/IS/Config.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IS
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            yield return new ApiResource("SwaggerAPI");
+            yield return new ApiResource("OrdersAPI");
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            yield return new IdentityResources.OpenId();
+            yield return new IdentityResources.Profile();
+        }
+
+        /// <summary>
+        /// IdentityServer4 version 4.x.x changes
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            yield return new ApiScope("SwaggerAPI", "Swagger API");
+            yield return new ApiScope("blazor", "Blazor WebAssembly");
+            yield return new ApiScope("OrdersAPI", "Orders API");
+        }
+
+        public static IEnumerable<Client> GetClients() =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "add_mvc",
+                ClientSecrets = { new Secret("add_mvc_secret".ToSha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+
+                AllowedScopes =
+                {
+                    "OrdersAPI",
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                },
+
+                RedirectUris = {"https://localhost:7272/signin-oidc"},
+                PostLogoutRedirectUris = {"https://localhost:7272/signout-callback-oidc"},
+
+                RequireConsent = false,
+
+                AccessTokenLifetime = 5,
+
+                AllowOfflineAccess = true
+
+                // AlwaysIncludeUserClaimsInIdToken = true
+            }
+        };
+    }
+}
---
+++ Test IS/MvcClient/Program.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, config =>
+    {
+        config.Authority = "https://localhost:5001";
+        config.ClientId = "add_mvc";
+        config.ClientSecret = "add_mvc_secret";
+        config.SaveTokens = true;
+        config.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+
+        config.ResponseType = "code";
+
+        config.Scope.Add("OrdersAPI");
+        config.Scope.Add("offline_access");
+
+        config.GetClaimsFromUserInfoEndpoint = true;
+
+        config.ClaimActions.MapJsonKey(ClaimTypes.DateOfBirth, ClaimTypes.DateOfBirth);
+    });
+
+builder.Services.AddAuthorization(config =>
+{
+    config.AddPolicy("HasDateOfBirth", builder =>
+    {
+        builder.RequireClaim(ClaimTypes.DateOfBirth);
+    });
+});
+
+builder.Services.AddHttpClient();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
