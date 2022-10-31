d4852918db6d9540eb4f32e989c6db25f85b70fb
Gleb <gleb.berezkin.2017@gmail.com>
2022-10-31 10:09:15 +0300

    Add jwt auth and IS

---
+++ ISWithJwtTokens/ISWithJwtTokens.IS/Quickstart/Account/JwtController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ISWithJwtTokens.IS.Quickstart.Account
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JwtController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public ActionResult Login([FromBody] JwtInputModel model)
+        {
+            var claim = new[]
+                {
+                new Claim(JwtRegisteredClaimNames.Sub, model.UserName)
+            };
+
+            var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            int expiryInMinutes = Convert.ToInt32(_configuration["Jwt:ExpiryInMinutes"]);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Site"],
+                audience: _configuration["Jwt:Site"],
+                expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+                signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+
+
+            return Ok(
+                new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+        }
+    }
+}
---
+++ ISWithJwtTokens/ISWithJwtTokens.IS/Quickstart/Account/JwtInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ISWithJwtTokens.IS.Quickstart.Account
+{
+    public class JwtInputModel
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
